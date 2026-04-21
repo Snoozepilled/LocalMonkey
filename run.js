cd5326d5f00b59361cf0964ed965daa321caf29c
Snoozepilled <xetanai@xetanai.moe>
2026-04-21 01:59:03 -0400

    Proof of concept

--- run.js
+++ run.js
@@ -1,1 +1,60 @@
-console.log("Hello, world!");
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const meta = require('./package.json');
+const config = require("./config/localmonkey");
+
+const app = express();
+
+app.use('/', (req, res, next) => {
+    console.log(`Handling request: ${req.url}`);
+    next();
+});
+
+// app.use('/script/userscript.js', (req, res) => {
+//     // TODO: Userscript configs that prefill data?
+//     let script = path.join(config.scriptPath, req.params.name);
+//     let content = fs.readFileSync(script);
+//     return res.status(200).send(content);
+// });
+
+app.get('/script/:name', (req, res) => {
+    console.log(`Serving script ${req.params.name}`);
+    res.setHeader('Content-Disposition', 'inline; filename="userscript.user.js"');
+    res.contentType('application/javascript'); 
+
+    let script = path.join(config.scriptPath, req.params.name);
+
+    if(!fs.existsSync(script)) {
+        return res.status(404).json({
+            message: `Script ${req.params.name} not found in ${config.scriptPath}`
+        });
+    };
+
+    try {
+        let content = fs.readFileSync(script);
+        return res.status(200).send(content);
+    } catch(err) {
+        return res.status(500).json({
+            message: 'Server error.',
+            err
+        });
+    };
+});
+
+app.get('/script', (req, res) => {
+    return res.status(400).json({
+        message: 'Missing script name.'
+    });
+});
+
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        version: meta.version
+    });
+});
+
+app.listen(config.port, () => {
+    console.log(`Listening on port ${config.port}.`);
+});
---
+++ scripts/userscript.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         LocalMonkey Client
+// @namespace    localmonkey
+// @version      1.0.0
+// @description  LocalMonkey Static Client Scipt.
+// @author       Snoozepilled
+// @match        https://www.example.com/*
+// @require      http://127.0.0.1:3000/script/example.js
+// @grant        none
+// ==/UserScript==
+
+console.log('LocalMonkey Client Script Enabled.');
